f2bc8433f08e02c50bcc1a8ada294e4990c61c5d
Alexis <alexispesicka@gmail.com>
2017-07-25 19:21:09 -0700

    Update slideshow.js

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,4 +1,4 @@
-var slideIndex = 0;
+var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -13,7 +13,7 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
+  if (n > slides.length) {slideIndex = 0} 
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none"; 
